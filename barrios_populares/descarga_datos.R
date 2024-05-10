c296eb0495aa93994bc1fbb15c4c4bcfd6d9e8e0
Martin Sferco <martinsferco.ms@gmail.com>
2024-05-09 23:23:13 -0300

    podemos descargar y cargar el archivo

--- barrios_populares/descarga_datos.R
+++ barrios_populares/descarga_datos.R
@@ -1,17 +1,27 @@
 # Instalo los paquetes necesarios (si aún no los tengo instalados)
+
+# Si hay problemas para instalar el paquete "googledrive" proba instalando
+# sudo apt-get install libcurl4-openssl-dev
+
 install.packages("googledrive") # No funciona la instalacion
 install.packages("readxl")
 
 library(googledrive)
+library(readxl)
+
+file_name = "Datos.xlsx"
 
 # Descargamos datos del Relevamiento habitacional desde Google Drive
-googledrive::drive_download(as_id("1IRhvzOQkvuspQF3TAsBCI-68i8ya0_hy/edit#gid=1908972294"),
+# El archivo se descarga en el workign directory. Despues lo eliminamos.
+googledrive::drive_download(as_id("1sD01MGvlotrAZuC_xPXwwNd5ipwqCHbo"),
                             overwrite = T)
 
 # Cargo el archivo como .xlsx
-datos <- readxl::read_excel("Datos_LP.xlsx", 
+datos <- readxl::read_excel(file_name, 
 														col_names = FALSE, 
 														skip = 3)
+# Borramos el archivo
+file.remove(file_name)
 
 # Veo la estructura del dataset
 str(datos)
